4facd595a263a07b2ed633081906b2ec51b7346f
lsb <leebutterman@gmail.com>
2017-02-12 22:24:58 +0000

    no sorting

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs'),
 
 xmlStream.on("tag:page", page => {
     if(page.revision.timestamp) page.revision = [page.revision]
-    page.revision.sort((a,b) => a.timestamp > b.timestamp).forEach(rev => {
+    page.revision.forEach(rev => {
         rev.page_id = page.id
         rev.page_title = page.title
         rev.page_ns = page.ns
