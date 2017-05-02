d131f598bb874118ee3cd79e703e02e65445c62a
lsb <leebutterman@gmail.com>
2017-05-02 07:58:54 +0000

    Massive overhaul; explicit sax stack, constant memory

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-var x = require('sax-stream')
-var stringify = require('json-stable-stringify')
-process.stdin.pipe(x({strict: true, tag: "page"})).on('data', (page) => {
-    pc = page.children
-    if(pc.revision.children) pc.revision = [pc.revision]
-    pc.revision.forEach(rev => {
-        rc = rev.children
-        cc = rc.contributor.children
-        h = {page_id: pc.id.value,
-             page_title: pc.title.value,
-             page_ns: pc.ns.value,
-             id: rc.id.value,
-             timestamp: rc.timestamp.value,
-             contributor: cc && {username: cc.username && cc.username.value, id: cc.id && cc.id.value, ip: cc.ip && cc.ip.value},
-             model: rc.model.value,
-             format: rc.format.value,
-             text: rc.text.value,
-             sha1: rc.sha1.value};
-        process.stdout.write(stringify(h))
-        process.stdout.write("\n")
-    })
-})
+"use strict";
+const sax = require("sax"), parser = sax.createStream(false, {trim: true});
+
+function backpressureStdout(s) {
+    if(!process.stdout.write(s, (() => process.stdin.resume())))
+        process.stdin.pause();
+}
+
+const m = "MEDIAWIKI", p = "PAGE", r = "REVISION", i = "ID", c = "CONTRIBUTOR",
+      rid = [m, p, r, i], rtime = [m, p, r, 'TIMESTAMP'], rtext = [m, p, r, 'TEXT'],
+      ruid = [m, p, r, c, i], runm = [m, p, r, c, 'USERNAME'], ruip = [m, p, r, c, 'IP'],
+      rpageid = [m, p, i], rpagetitle = [m, p, 'TITLE'], rpagens = [m, p, 'NS'];
+const watchedPaths = {[rid.join()]: "rid", [rtime.join()]: "rtime", [rtext.join()]: "rtext",
+                      [ruid.join()]: "ruid", [runm.join()]: "runm", [ruip.join()]: "ruip",
+                      [rpageid.join()]: "rpageid", [rpagetitle.join()]: "rpagetitle", [rpagens.join()]: "rpagens"};
+const whenToWriteOut = [m,p,r].join();
+const whenToClear = [m,p].join();
+const stack = [];
+const revisionDetails = {};
+
+parser.ontext = function(t) {
+    const maybeIdx = watchedPaths[stack.join()];
+    if(maybeIdx)
+        revisionDetails[maybeIdx] = t;
+}
+
+parser.onopentag = ((node) => stack.push(node.name));
+
+parser.onclosetag = function() {
+    if(whenToWriteOut === stack.join()) {
+        backpressureStdout(JSON.stringify(revisionDetails)+"\n");
+        ["rid", "rtime", "ruid", "runm", "ruip"].forEach((idx) => revisionDetails[idx] = null);
+    }
+    if(whenToClear === stack.join()) {
+        ["rpageid", "rpagetitle", "rpagens"].forEach((idx) => revisionDetails[idx] = null);
+    }
+    stack.pop();
+}
+
+process.stdin.pipe(parser);
