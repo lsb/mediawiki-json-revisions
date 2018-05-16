dbf23485483906977cee324d016c9b4ce5da4818
lsb <leebutterman@gmail.com>
2018-05-16 01:01:08 -0700

    Also save the sha1.

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ function backpressureStdout(s) {
 const m = "MEDIAWIKI", p = "PAGE", r = "REVISION", i = "ID", c = "CONTRIBUTOR",
       rid = [m, p, r, i], rtime = [m, p, r, 'TIMESTAMP'], rtext = [m, p, r, 'TEXT'],
       ruid = [m, p, r, c, i], runm = [m, p, r, c, 'USERNAME'], ruip = [m, p, r, c, 'IP'],
+      rpagesha1 = [m, p, r, "SHA1"],
       rpageid = [m, p, i], rpagetitle = [m, p, 'TITLE'], rpagens = [m, p, 'NS'];
 const watchedPaths = {[rid.join()]: "rid", [rtime.join()]: "rtime", [rtext.join()]: "rtext",
                       [ruid.join()]: "ruid", [runm.join()]: "runm", [ruip.join()]: "ruip",
+                      [rpagesha1.join()]: "rpagesha1",
                       [rpageid.join()]: "rpageid", [rpagetitle.join()]: "rpagetitle", [rpagens.join()]: "rpagens"};
 const whenToWriteOut = [m,p,r].join();
 const whenToClear = [m,p].join();
@@ -29,7 +31,7 @@ parser.onopentag = ((node) => stack.push(node.name));
 parser.onclosetag = function() {
     if(whenToWriteOut === stack.join()) {
         backpressureStdout(JSON.stringify(revisionDetails)+"\n");
-        ["rid", "rtime", "ruid", "runm", "ruip"].forEach((idx) => revisionDetails[idx] = null);
+        ["rid", "rtime", "ruid", "runm", "ruip", "rpagesha1"].forEach((idx) => revisionDetails[idx] = null);
     }
     if(whenToClear === stack.join()) {
         ["rpageid", "rpagetitle", "rpagens"].forEach((idx) => revisionDetails[idx] = null);
